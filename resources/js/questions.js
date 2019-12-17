3b8f7db3a9ffe8e3759c93a3d05788e8ea392471
lil L <57925562+leolopez10@users.noreply.github.com>
2019-12-16 21:37:35 -0600

    created questions.

---
+++ resources/js/questions.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "style", "src"],
+        answer: "style"
+    }]
--- resources/js/script.js
+++ resources/js/script.js
@@ -14,3 +14,4 @@ startBtn.addEventListener("click", function () {
 })
 
 
+
