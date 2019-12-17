cb198c10b6eaf8059d79a13f6c686db0a910f014
lil L <57925562+leolopez10@users.noreply.github.com>
2019-12-16 21:11:44 -0600

    Added html and a countdown timer after clicking the start button.

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+var secondsEl = document.querySelector("#seconds");
+var startBtn = document.querySelector("#startButton");
+
+
+startBtn.addEventListener("click", function () {
+    var timeLeft = 75;
+    var countDownTimer = setInterval(function () {
+        secondsEl.innerHTML = timeLeft;
+        timeLeft -= 1;
+        if (timeLeft <= 0) {
+            clearInterval(countDownTimer);
+        }
+    }, 1000);
+})
+
+
